45fcdd8b9cf48cebea7ec1f9c67ab030779a3669
Terry Yu <tyu14@mail.depaul.edu>
2018-05-30 15:01:31 -0500

    Did some work

--- projectLottery.sol
+++ projectLottery.sol
@@ -2,23 +2,36 @@ pragma solidity ^0.4.22;
 
 contract projectLottery{
     
+    
+    //Some unnecessary variables, clean up later
     address public owner; //Contract owner
-    address[5] public played; //Array to store all players
+    //address[5] public played; //Array to store all players
     uint256 public winningNumber; //Winning number to be determined
+    uint256 private fee;
     uint256 public pot; //Total amount in lottery
+    uint private feePot;
     uint public numPlayed = 0; //Players so far
+    uint256 endTime;
+    uint256 startTime;
 
-    constructor() public{
+    constructor(uint256 _time, uint256 _fee) public{
         owner = msg.sender;
+        fee = _fee;
+        //startTime = getTime();
+        endTime = getTime() + _time;
     }
 
-    struct Player{
+   /* struct Player{
         address playerAddress;    
         bytes32 hash;
+    }*/
+    
+    function showTimeLeft() public returns(uint256){
+        return (endTime - startTime); //Doesn't seem to work at the moment
     }
+    
 
-    mapping(address => Player) players;
-    address[] public playersList;
+    mapping(address => bytes32) public players;
 
     function play (bytes32 hash) external payable {
         require(msg.value == 2 ether,
@@ -26,23 +39,13 @@ contract projectLottery{
         );
 
         require (
-          numPlayed < played.length,
-          "Maximum number of players reached already"
+          players[msg.sender] == 0
         );
 
-        pot += msg.value;
-        addPlayer (msg.sender, hash);
+        players[msg.sender] = hash; //Similar to addPlayer()
+        pot += msg.value-fee;
+        feePot += fee;
     }
-
-    function addPlayer(address _playerAddress, bytes32 _hash) private{
-        var player = playersList[_playerAddress];
-
-        player.playerAddress = _playerAddress;
-        player.hash = _hash;
-
-        playersList.push(_playerAddress);
-    }
-    
     
     function setWinningNumber (uint256 _winning_number) external {
         require(msg.sender == owner); //Only contract owner can set winning number
@@ -59,6 +62,10 @@ contract projectLottery{
 
     function done () external {
         
+    }
+    
+    function getTime() returns(uint256){
+        return now;
     }
 
     /*Functions that work but are not needed. Testing purposes
