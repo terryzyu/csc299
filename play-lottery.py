46f3553ad063615bc61b10a19047ae85ad2089de
Terry Yu <terryzyu@gmail.com>
2018-05-31 18:55:15 -0500

    More work done

--- play-lottery.py
+++ play-lottery.py
@@ -11,22 +11,28 @@ with open ("abi.json") as f:
 
 contract_address = sys.argv[1]
 account_index = int (sys.argv[2])
+value = int (sys.argv[3]) #ether amount to send
 account = web3.eth.accounts[account_index]
 print ("Using account {:d} with address {:s} to play on contract {:s}".format (account_index, account, contract_address))
 contract = web3.eth.contract (abi = abi, address = contract_address)
-playersInfo = dict()
 
-strRandom = str(random.random()) #Gets random number and converts to string
-hash = "0x"+hashlib.sha256(strRandom.encode('utf-8')).hexdigest() #Converts the random string to hash
-
-
+#Gets a random number and hashes it
+randomNum = random.getrandbits(256)
+hash = "0x" + hashlib.sha256((randomNum).to_bytes(32, byteorder='big', signed=False)).hexdigest()
 
 transaction_hash = contract.transact ({
     "from": account,
-    "value": web3.toWei (0.1, "ether")
+    "value": web3.toWei (value, "ether")
 }).play (hash);
 
 print ("transaction hash = {:s}".format (transaction_hash))
 transaction_receipt = web3.eth.getTransactionReceipt (transaction_hash)
 transaction = web3.eth.getTransaction (transaction_hash)
 pprint (dict (transaction_receipt))
+
+#Creates a file to store the player's address and their random number.
+#Will create file if none exist, otherwise it'll append
+#Used later in reveal. 
+playerInfo = account + " " + randomNum + "\n"
+with open('playerInfo.txt', 'a') as file:
+    file.write(playerInfo)
--- projectLottery.sol
+++ projectLottery.sol
@@ -13,6 +13,7 @@ contract projectLottery{
     uint public numPlayed = 0; //Players so far
     uint256 endTime;
     uint256 startTime;
+    bytes32 winningHash;
 
     constructor(uint256 _time, uint256 _fee) public{
         owner = msg.sender;
@@ -27,36 +28,45 @@ contract projectLottery{
     }*/
     
     function showTimeLeft() public returns(uint256){
-        return (endTime - startTime); //Doesn't seem to work at the moment
+        return (endTime - getTime()); //Doesn't seem to work at the moment
+        //okay it works now :^)
     }
     
 
     mapping(address => bytes32) public players;
+    mapping(address => bytes32) public winners;
 
     function play (bytes32 hash) external payable {
-        require(msg.value == 2 ether,
-            "Player must send exactly 2 ether"
+        require(msg.value == 5 ether,
+            "Player must send exactly 5 ether"
         );
 
         require (
-          players[msg.sender] == 0
+          players[msg.sender] == 0 //Cannot play twice
         );
 
         players[msg.sender] = hash; //Similar to addPlayer()
-        pot += msg.value-fee;
-        feePot += fee;
+        pot += msg.value-fee; //Pays fee to contract owner
+        feePot += fee; //Adds remaining value to pot
+        numPlayed++;
     }
     
     function setWinningNumber (uint256 _winning_number) external {
+        require(getTime() >= endTime, "End time hasn't been passed"); //Can only be called after ending
         require(msg.sender == owner); //Only contract owner can set winning number
         winningNumber = _winning_number;
+        winningHash = sha256(_winning_number);
         
     }
 
-    function reveal (uint256 r) external {
-        
-        bytes32 h = sha256 (winningNumber, r);
-        //require (played[msg.sender] == h);
+    function reveal (uint256 r) external returns(bytes32){
+        require(getTime() >= endTime); //Can only be called after ending
+        bytes32 h = sha256 (r);//Calculates sha256 of number sent in by player
+        return h;
+       /* require (players[msg.sender] == h);
+        if(h == winningHash){
+            winners[msg.sender] = h;
+        }*/
         
     }
 
